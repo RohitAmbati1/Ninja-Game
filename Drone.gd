17a2f87befeade1da81919d179610b84c8e4dbd1
RohitAmbati1 <ramenaimbot@gmail.com>
2022-07-06 19:04:35 -0500

    drones change color

--- Drone.gd
+++ Drone.gd
@@ -9,7 +9,8 @@ onready var drone_attack = $Sprite_LitUp
 var speed: int = 50
 var rng = RandomNumberGenerator.new()
 
-var phase = "move"
+var phase = "move" setget set_phase
+
 var destination: Vector2
 
 
@@ -30,9 +31,45 @@ func _physics_process(delta):
 		var movement: Vector2 = self.destination - self.global_position
 		if movement.length() < self.speed*delta:
 			self.global_position = self.destination
-			self.phase == "attack"
+			self.phase = "attack"
 			
 		else:
 			movement = movement.normalized()*self.speed
+# warning-ignore:unused_variable
 			var velocity = self.move_and_slide(movement)
 		self.look_at(self.player.global_position)
+
+func set_phase(new_phase):
+	if new_phase == phase:
+		return
+		
+	var timer = Timer.new()
+	timer.autostart = true
+	timer.wait_time = 0.5
+	add_child(timer)
+	timer.connect("timeout", self, "prepare")
+	
+func prepare():
+	var timer = self.get_node("Timer")
+	#change sprite
+	self.drone.visible = false
+	self.drone_attack.visible = true
+	#restart timer
+	timer.start()
+	#disconnect timer from prepare
+	timer.disconnect("timeout", self, "prepare")
+	#connect timer to shoot function
+	timer.connect("timeout", self, "attack")
+	#make shoot function
+func attack():
+	#make the bullets
+	#tell them where to go
+	#add them to the main scene tree
+	#reset timer to trigger moving again
+	pass
+	
+func move_again():
+	#remove the timer from drone
+	self.phase = "move"
+	pass
+	
